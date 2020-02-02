2434dc738334d6e782052d0036452bd9332da88a
JUNG Myoungki <myoungki.jung@gmail.com>
2020-02-02 11:17:29 +1000

    two joints

--- src/rrbot_cmd.py
+++ src/rrbot_cmd.py
@@ -5,15 +5,20 @@ from std_msgs.msg import Float64
 import math
  
 def joint2_controller():
-    pub = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size=10)
-    rospy.init_node('joint2_controller', anonymous=False)
-    rate = rospy.Rate(50) # 10hz
+    pub1 = rospy.Publisher('/rrbot/joint1_position_controller/command', Float64, queue_size=10)
+    pub2 = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size=10)
+    rospy.init_node('rrbot_joint_controller', anonymous=False)
+    rate = rospy.Rate(50) # 50hz
     count_i = 0.0
     while not rospy.is_shutdown():
         count_i += 1.0
-        position = (math.pi/4)+(1*math.pi/4)*math.sin(count_i/40.0)*math.sin(count_i/40.0)
-        # rospy.loginfo(position)
-        pub.publish(position)
+        position1 = (1.0*math.pi/4.0)*math.sin(count_i/90.0)*math.sin(count_i/90.0)
+        position2 = (math.pi/4.0)-(1.0*math.pi/4.0)*math.cos(count_i/40.0)*math.cos(count_i/40.0)
+        # rospy.loginfo(position1)
+        pub1.publish(position1)
+        # rospy.loginfo(position2)
+        pub2.publish(position2)
+
         rate.sleep()
  
 if __name__ == '__main__':
