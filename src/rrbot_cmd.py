5ed27aa1efa7c6a28fbe24bdd38e665a60decaee
JUNG Myoungki <myoungki.jung@gmail.com>
2020-02-02 11:06:55 +1000

    faster joint move

--- src/rrbot_cmd.py
+++ src/rrbot_cmd.py
@@ -7,11 +7,11 @@ import math
 def joint2_controller():
     pub = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size=10)
     rospy.init_node('joint2_controller', anonymous=False)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(50) # 10hz
     count_i = 0.0
     while not rospy.is_shutdown():
         count_i += 1.0
-        position = (math.pi/4)+(1*math.pi/4)*math.sin(count_i/20.0)*math.sin(count_i/20.0)
+        position = (math.pi/4)+(1*math.pi/4)*math.sin(count_i/40.0)*math.sin(count_i/40.0)
         # rospy.loginfo(position)
         pub.publish(position)
         rate.sleep()
